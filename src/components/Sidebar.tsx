9f5f0ffc0795baafef63793f74244772cf0123c6
Rhino-ty <okko8522@gmail.com>
2023-03-04 17:34:47 +0900

    feat: 사이드바 메뉴 NavLink 연결 및 css 조정

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,35 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const SidebarContainer = styled.div`
+  width: 20%;
+  height: 91vh;
+  padding: 20px 30px;
+  border-right: 0.5px solid #000;
+
+  nav {
+    font-size: 1.2em;
+
+    .selected {
+      color: #3434ff;
+    }
+  }
+`;
+
 export default function Sidebar() {
-  return <></>;
+  return (
+    <SidebarContainer>
+      <nav>
+        <NavLink to="/a" className={({ isActive }) => (isActive ? "selected" : "not")}>
+          Page A
+        </NavLink>
+        <NavLink to="/b" className={({ isActive }) => (isActive ? "selected" : "not")}>
+          Page B
+        </NavLink>
+        <NavLink to="/c" className={({ isActive }) => (isActive ? "selected" : "not")}>
+          Page C
+        </NavLink>
+      </nav>
+    </SidebarContainer>
+  );
 }
