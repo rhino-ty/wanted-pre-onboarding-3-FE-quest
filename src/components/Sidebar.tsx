f4c5fc81b866c89b83041ad329c0f53b5cbbece8
Rhino-ty <okko8522@gmail.com>
2023-03-05 11:41:34 +0900

    style: 사이드바 반응형 및 디자인 수정

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,13 +2,25 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const SidebarContainer = styled.div`
+  @media screen and (max-width: 768px) {
+    padding: 10px 20px;
+  }
   width: 20%;
   height: 91vh;
-  padding: 20px 30px;
+  padding: 10px 30px;
   border-right: 0.5px solid #000;
 
   nav {
+    @media screen and (max-width: 768px) {
+      font-size: 1em;
+    }
     font-size: 1.2em;
+    display: flex;
+    flex-direction: column;
+
+    a {
+      padding: 10px 0;
+    }
 
     .selected {
       color: #3434ff;
