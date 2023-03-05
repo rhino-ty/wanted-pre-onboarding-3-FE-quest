6ff3d38fe7c42d034662f4921c3b22b13bfb2b19
Rhino-ty <okko8522@gmail.com>
2023-03-05 12:17:31 +0900

    feat: Header 내용 채우기, 디자인

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,20 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  border-bottom: 0.5px solid #000;
+  padding: 20px;
+  align-items: center;
+
+  h1 {
+    font-style: italic;
+    font-weight: 500;
+  }
+`;
+
 export default function Header() {
-  return <></>;
+  return (
+    <HeaderContainer>
+      <h1>Wanted Pre-onboarding course</h1>
+    </HeaderContainer>
+  );
 }
