3adaaf5f8b1ff07ba550960909fef5b440fe8142
Rhino-ty <okko8522@gmail.com>
2023-03-06 12:43:17 +0900

    style: 사이드바, 메인 섹션 수평으로 구조 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import A from "./page/A";
 import B from "./page/B";
 import C from "./page/C";
 
+const MainSection = styled.section`
+  display: flex;
+`;
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <Sidebar />
-      <Routes>
-        <Route path="/a" element={<A />} />
-        <Route path="/b" element={<B />} />
-        <Route path="/c" element={<C />} />
-      </Routes>
+      <MainSection>
+        <Sidebar />
+        <Routes>
+          <Route path="/a" element={<A />} />
+          <Route path="/b" element={<B />} />
+          <Route path="/c" element={<C />} />
+        </Routes>
+      </MainSection>
     </div>
   );
 }
