0d184618d296f9e16f6b7c45805438c9e5ce534a
Rhino-ty <okko8522@gmail.com>
2023-03-06 13:00:08 +0900

    style: 페이지 content 디자인 수정

--- src/page/B.tsx
+++ src/page/B.tsx
@@ -1,3 +1,14 @@
+import styled from "styled-components";
+
+const ContentContainer = styled.div`
+  width: 100%;
+  color: #3434ff;
+  font-size: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default function B() {
-  return <>B</>;
+  return <ContentContainer>B</ContentContainer>;
 }
